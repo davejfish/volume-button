d673a108d8191863de110210d9904df46cdd98c6
Dave Fisher <dave.j.fish@gmail.com>
2022-06-10 12:20:40 -0700

    working volume button

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 // state
-
+let volume = 10;
 
 // component
     // define and grab DOM elements
@@ -10,6 +10,33 @@
         // logic and calculations
         // state update
         // re-display components
+const volumeBox = document.querySelector('section');
+const [down, up] = volumeBox.querySelectorAll('button');
+const volumeDisplay = volumeBox.querySelector('span');
 
+function updateVolume() {
+    volumeDisplay.textContent = volume;
+}
+
+down.addEventListener('click', () => {
+    if (volume < 1) {
+        return;
+    } 
+    else {
+        volume--;
+        volumeDisplay.textContent = volume;
+    }
+});
+
+up.addEventListener('click', () => {
+    if (volume >= 20) {
+        return;
+    } 
+    else {
+        volume++;
+        volumeDisplay.textContent = volume;
+    }
+});
 
 // page load actions
+updateVolume();
